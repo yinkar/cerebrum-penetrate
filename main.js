37cb7749072e2408632fa2271d200c56fd4b3f46
Hüseyin <hkrabs@yandex.com>
2023-04-19 09:23:19 +0300

    Ctrl + Enter to interprete support

--- main.js
+++ main.js
@@ -5,8 +5,24 @@ import CerebrumPenetrate from './src/lib/CerebrumPenetrate';
 const b = new CerebrumPenetrate();
 b.setOutputElement('.output');
 
-document.querySelector('.interprete').addEventListener('click', () => {
-  b.load(document.querySelector('#input').value);
+const interprete = () => {
+  b.load(document.querySelector('.input').value);
   b.run();
   b.print();
+};
+
+document.querySelector('.interprete').addEventListener('click', interprete);
+
+const keyCombination = [];
+
+document.querySelector('.input').addEventListener('keydown', e => {
+  keyCombination.push(e.code);
+
+  if (keyCombination.length === 2 && keyCombination.includes('ControlLeft') && keyCombination.includes('Enter')) {
+    interprete();
+  }
+});
+
+document.querySelector('.input').addEventListener('keyup', e => {
+  keyCombination.splice(keyCombination.indexOf(e.code), 1);
 });
